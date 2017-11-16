07564cb2d7f91305414858e936a769509950ccf8
Hung Nguyen Ba <hung.nb@temando.com>
2017-11-17 00:39:08 +0700

    finished refactor component

--- src/components/ContestPreview.js
+++ src/components/ContestPreview.js
@@ -1,17 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export default class ContestPreview extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    console.log(this.props.contestName);
+  }
+
   render() {
-    const contest = this.props;
+    const { categoryName, contestName } = this.props;
     return (
-      <div className="ContestPreview">
+      <div className="link ContestPreview" onClick={this.handleClick}>
         <div className="category-name">
-          {contest.categoryName}
+          {categoryName}
         </div>
         <div className="contest-name">
-          {contest.contestName}
+          {contestName}
         </div>
       </div>
     );
   }
 }
+
+ContestPreview.propTypes = {
+  categoryName: PropTypes.string.isRequired,
+  contestName: PropTypes.string.isRequired
+};
